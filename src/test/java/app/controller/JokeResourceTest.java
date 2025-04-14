f8002a2d4177aa0e45958d6aaaf0115e8a6b7e4f
Philippe BERNARD <filhype.bernard@gmail.com>
2025-04-14 21:28:14 +0200

    ajout token tu

--- src/test/java/app/controller/JokeResourceTest.java
+++ src/test/java/app/controller/JokeResourceTest.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import app.entity.Joke;
 import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,6 +12,27 @@ import static org.hamcrest.Matchers.containsString;
 class JokeResourceTest {
 
     static final String basePath = "/api/jokes/v1";
+
+    @ConfigProperty(name = "quarkus.oidc.client-id")
+    String clientId;
+
+    @ConfigProperty(name = "quarkus.oidc.credentials.secret")
+    String clientSecret;
+
+    @ConfigProperty(name = "test-url-keycloak")
+    String authServerUrl;
+
+    protected String GetAccessToken() {
+        return "Bearer " + given().contentType("application/x-www-form-urlencoded")
+                .formParam("grant_type", "client_credentials")
+                .formParam("client_id", clientId)
+                .formParam("client_secret", clientSecret)
+                .when()
+                .post(authServerUrl)
+                .then()
+                .statusCode(200)
+                .extract().path("access_token");
+    }
 
     @Test
     void getRandomJoke() {
@@ -38,8 +60,10 @@ class JokeResourceTest {
     @Test
     void addJoke() {
         String newJoke = "This is a test joke";
+        String token = GetAccessToken();
 
         given().basePath(basePath)
+                .header("Authorization", token)
                 .body(String.format("{\"joke\":\"%s\"}", newJoke))
                 .contentType("application/json")
                 .when()
@@ -52,10 +76,13 @@ class JokeResourceTest {
     @Test
     void updateJoke() {
         String newJoke = "This is a test2 joke";
+        String token = GetAccessToken();
+
         Joke j  = Joke.findAll().firstResult();
         j.joke = newJoke;
 
         given().basePath(basePath)
+                .header("Authorization", token)
                 .body(j)
                 .contentType("application/json")
                 .when()
@@ -68,9 +95,11 @@ class JokeResourceTest {
     @Test
     void deleteJoke() {
         Joke j  = Joke.findAll().firstResult();
+        String token = GetAccessToken();
 
         given()
                 .when().basePath(basePath)
+                .header("Authorization", token)
                 .pathParams("id", j.id)
                 .delete("{id}")
                 .then()
